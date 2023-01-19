b3fe367117c9648d22185e16277e73aa6ccb18a7
Curtis <cmbitton@gmail.com>
2023-01-19 15:57:19 -0500

    Added images to project source

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import _ from 'lodash';
 import myName from './myName';
 import './style.css';
+import Icon from './icon.jpg'
 
 function component() {
-    const element = document.createElement('div');
-    const element2 = document.createElement('div');
+  const element = document.createElement('div');
+  const element2 = document.createElement('div');
 
-    element2.textContent = myName('Curtis');
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return {element, element2};
-  }
-  
-  document.body.appendChild(component().element);
-  document.body.appendChild(component().element2)
+  element2.textContent = myName('Curtis');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return { element, element2 };
+}
+
+document.body.appendChild(component().element);
+document.body.appendChild(component().element2)
